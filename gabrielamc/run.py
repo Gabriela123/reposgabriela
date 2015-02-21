304eff6dcba18c31676d94a91d71ebf1c224519d
rfedmi <Dav1dmp>
2015-02-21 18:05:15 -0500

    Segundo cambio en el proyecto de gabriela

--- gabrielamc/run.py
+++ gabrielamc/run.py
@@ -6,6 +6,9 @@ Created on 19/1/2015
 
 Gabriela Castro
 Primer Proyecto de Python con Github
+
+
+Segundo cambio en proyecto de Gabriela... por david
 '''
 from app import app
 if __name__ == '__main__':
